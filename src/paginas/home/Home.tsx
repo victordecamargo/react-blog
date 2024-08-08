47622eb6dc9170510963c679981d9fb0c58497f5
Victor Rodrigues de Camargo <victorvii.camargo@hotmail.com>
2024-08-08 14:05:25 -0300

    React Router Dom

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import './Home.css';
+import { useNavigate, Link } from 'react-router-dom'
+import React from 'react'
 
-function Home(){
-    return(
-        <>
-            <h1 className='titulo'>Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial"className="img"></img>
 
-        </>
-    );
+
+const Home = () => {
+    let navigate = useNavigate()
+    return (
+
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
+
+        </div>
+
+    )
 }
 
-export default Home;
+export default Home
