62b8420450701ce6da4a72811cfe95042b20b3ed
Victor Rodrigues de Camargo <victorvii.camargo@hotmail.com>
2024-08-15 15:17:07 -0300

    add perfil

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+
 import homeLogo from '../../assets/home.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
+
 
 function Home() {
     return (
@@ -8,13 +12,13 @@ function Home() {
         <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
@@ -23,7 +27,7 @@ function Home() {
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }
